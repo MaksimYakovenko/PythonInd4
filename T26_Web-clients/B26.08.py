e1dc6a605d220f1c8cceb4c5b199df09f1a4c596
Maksim <103666491+MaksimYakovenko@users.noreply.github.com>
2023-02-03 08:40:52 +0200

    T26

--- T26_Web-clients/B26.08.py
+++ T26_Web-clients/B26.08.py
@@ -4,6 +4,7 @@ import re
 HUMIDITY = r'<span title="Вологість">(.*?)</span>'
 TEMP = r'<span class="today-hourly-weather__temp">(.*?)</span>'
 DATE = r'<div class="day-in-history__date">(.*?)</div>'
+PERIOD = r'<span class="today-hourly-weather__name">(.*?)</span>'
 
 def find_humidity(html):
     re_list = re.findall(HUMIDITY, html, re.DOTALL)
@@ -15,6 +16,10 @@ def find_temp(html):
 
 def find_date(html):
     re_list = re.findall(DATE, html, re.DOTALL)
+    return re_list
+
+def find_period(html):
+    re_list = re.findall(PERIOD, html, re.DOTALL)
     return re_list
 
 def make_url(city):
@@ -29,12 +34,13 @@ def main_function(city):
     url = make_url(city)
     html = get_html(url)
 
-    humidity_list, temp_list, date_list = find_humidity(html), find_temp(html), find_date(html)
-    # print(date_list)
+    humidity_list, temp_list, date_list, period_list = find_humidity(html), find_temp(html), find_date(html), find_period(html)
+    print(date_list[0])
     # print(humidity_list)
     # print(temp_list)
+    # print(period_list)
     for i in range(1, 5):
-        print(humidity_list[i - 1] + "  " + temp_list[i - 1] + "  " + date_list[i - 3])
+        print(period_list[i - 1] + "   " + temp_list[i - 1] + "   " + humidity_list[i - 1] )
 
 if __name__ == '__main__':
-    main_function("Kyiv")  
+    main_function("Kyiv")
