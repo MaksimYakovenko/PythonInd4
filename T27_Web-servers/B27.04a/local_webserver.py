657b626f0f44cdcd0b3eab896d75d765c459a5dc
Maksim <103666491+MaksimYakovenko@users.noreply.github.com>
2023-02-06 16:56:43 +0200

    T27

---
+++ T27_Web-servers/B27.04a/local_webserver.py
@@ -0,0 +1,8 @@
+from http.server import HTTPServer, CGIHTTPRequestHandler
+
+HOST = ""
+PORT = 8000
+
+if __name__ == '__main__':
+    print("=== Local webserver ===")
+    HTTPServer((HOST, PORT), CGIHTTPRequestHandler).serve_forever()
