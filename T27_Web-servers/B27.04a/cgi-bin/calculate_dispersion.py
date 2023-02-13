cc147a1f520c9d257b75abc303b0536a5e9191e5
Maksim <103666491+MaksimYakovenko@users.noreply.github.com>
2023-02-13 17:18:02 +0200

    T27

--- T27_Web-servers/B27.04a/cgi-bin/calculate_dispersion.py
+++ T27_Web-servers/B27.04a/cgi-bin/calculate_dispersion.py
@@ -1,22 +1,25 @@
 import cgi
 from string import Template
 
-
-def calculate_dispersion(string):
-    arith_mean = sum(string) / len(string)
-    diff = [(v - arith_mean) for v in string]
-    sqr_diff = [d ** 2 if d < 0 else d for d in diff]
-    return sqr_diff
+def calculate_dispersion(str1):
+    lst = [float(k) for k in str1.split(",")]
+    arith_mean = sum(lst) / len(lst) if len(lst) != 0 else 0
+    diff = [(v - arith_mean) for v in lst]
+    mod_diff = [abs(d) if d < 0 else d for d in diff]
+    return mod_diff
 
 if __name__ == '__main__':
     form = cgi.FieldStorage()
-    string = form.getfirst("string", "")
+    string = form["string"].value
     result = calculate_dispersion(string)
+    # answer = 'обчислена дисперсія' if calculate_dispersion(string) else \
+    #     'дисперсію неможливо обчислити'
+    #
+    # result = string + ' - ' + answer
     with open("resultpage.html", encoding="utf-8") as f:
-        page = Template(f.read().substitute(result=result))
+        page = Template(f.read()).substitute(result=result)
 
     import os
-
     if os.name == "nt":
         import sys, codecs
         sys.stdout = codecs.getwriter("utf-8")(sys.stdout.buffer)
