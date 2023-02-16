20c618ab6c815ae4c7e3306f6aba1e15b7438c06
Maksim <103666491+MaksimYakovenko@users.noreply.github.com>
2023-02-16 21:37:37 +0200

    T27

--- T27_Web-servers/B27.04b/main.py
+++ T27_Web-servers/B27.04b/main.py
@@ -8,8 +8,6 @@ def incorrect_data(string):
     s = re.findall(INCORRECT_DATA, string)
     return s
 
-
-
 def calculate_dispersion(string):
     lst_value = [float(k) for k in string.split(",")]
     arith_mean = sum(lst_value) / len(lst_value) if len(lst_value) != 0 else 0
@@ -17,7 +15,6 @@ def calculate_dispersion(string):
     mod_diff = [abs(d) if d < 0 else d for d in diff]
     mod_diff_str = ', '.join(map(str, mod_diff))
     return mod_diff_str
-
 
 def application(environ, start_response):
     if environ.get("PATH_INFO", "").lstrip("/") == "":
@@ -36,19 +33,15 @@ def application(environ, start_response):
                 answer = "обчислена дисперсія"
                 result = "<h1>{} - {}</h1>".format(calculate_dispersion(
                     string), answer)
-
         start_response("200 OK", [("Content-type", "text/html; charset=utf-8"), ])
         with open("templates/dispersion.html", encoding="utf-8") as f:
             page = Template(f.read()).substitute(result=result)
-
-
     else:
         start_response("404 NOT FOUND", [("Content-type", "text/html; charset=utf-8"), ])
         with open("templates/error_404.html", encoding="utf-8") as f:
             page = f.read()
 
     return [bytes(page, encoding="utf-8")]
-
 
 HOST = ""
 PORT = 8000
