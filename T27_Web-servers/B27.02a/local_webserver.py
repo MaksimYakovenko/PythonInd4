8eee1e5a6adc0261ea2b6fa77d1014c3b0275f54
Maksim <103666491+MaksimYakovenko@users.noreply.github.com>
2023-05-22 20:07:17 +0300

    T27

---
+++ T27_Web-servers/B27.02a/local_webserver.py
@@ -0,0 +1,8 @@
+from http.server import HTTPServer, CGIHTTPRequestHandler
+
+HOST = ""
+PORT = 8005
+
+if __name__ == '__main__':
+    print(f'Локальний веб-сервер запущено на http://localhost:{PORT}')
+    HTTPServer((HOST, PORT), CGIHTTPRequestHandler).serve_forever()
