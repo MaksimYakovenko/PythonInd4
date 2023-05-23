1c05c9d065e84076752369c3638b8d5482ef6d5a
Maksim <103666491+MaksimYakovenko@users.noreply.github.com>
2023-05-23 16:55:43 +0300

    T27

---
+++ T27_Web-servers/B27.11a/cgi-bin/main.py
@@ -0,0 +1,58 @@
+import cgi
+from string import Template
+
+
+def find_longest_sequence(string):
+    if not string:
+        return None, None
+
+    current_char = ""
+    current_length = 0
+    max_char = ""
+    max_length = 0
+
+    for char in string:
+        if char == current_char:
+            current_length += 1
+        else:
+            current_char = char
+            current_length = 1
+
+        if current_length > max_length:
+            max_char = current_char
+            max_length = current_length
+
+    return max_char, max_length
+
+
+def highlight_longest_sequence(string, max_char):
+    highlighted_string = ""
+    for char in string:
+        if char == max_char:
+            highlighted_string += '<span style="color: blue;">{}</span>'.format(char)
+        else:
+            highlighted_string += char
+    return highlighted_string
+
+
+if __name__ == '__main__':
+    form = cgi.FieldStorage()
+    string = form.getfirst("string")
+    max_char, max_length = find_longest_sequence(string)
+    if max_char is not None and max_length is not None:
+        highlighted_string = highlight_longest_sequence(string, max_char)
+        result = "<h1>{}</h1>".format(highlighted_string)
+    else:
+        result = ""
+
+    with open("result.html", encoding="utf-8") as f:
+        page = Template(f.read()).substitute(result=result)
+
+    import os
+
+    if os.name == "nt":
+        import sys, codecs
+
+        sys.stdout = codecs.getwriter("utf-8")(sys.stdout.buffer)
+    print("Content-type: text/html charset=utf-8\n")
+    print(page)
